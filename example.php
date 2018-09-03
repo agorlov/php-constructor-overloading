443c84a2785059f671fe9ae4660ed622379a2d55
Alexandr Gorlov <a.gorlov@gmail.com>
2018-09-03 22:12:12 +0300

    sample code

---
+++ example.php
@@ -0,0 +1,42 @@
+<?php
+
+class DM extends DateTime {
+}
+
+class CashOverloaded {
+        private $kop;
+
+        private function constrRubB(boolean $rub) {
+                echo "constrRub boolean!\n";
+        }
+
+        private function constrRubD(double $rub, integer $kop) {
+                echo "constrRub float!\n";
+        }
+
+        private function constrRubDate(float $rub, DateTime $dm) {
+                echo "constrRub float!\n";
+                $this->__construct($rub);
+        }
+
+        private function constrRub(float $rub) {
+                echo "constrRub float!\n";
+        }
+
+        private function constrPrimary(int $kop) {
+                echo "constrPrimary int!\n";
+        }
+
+        public function __construct() {
+                $method = (new OverloadedConstructor($this, func_get_args()))->construct()->getName();
+                echo "Method: $method!\n";
+                $this->$method(...func_get_args());
+        }
+
+        public function money(): string {
+        }
+}
+
+
+//new CashOverloaded(1.1, new DM);
+new CashOverloaded(true);
